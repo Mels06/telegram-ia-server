d7c75bb1ad9760638210140f42042a79af29c818
Mélissa Delome <melissadelome@gmail.com>
2026-02-04 22:54:30 +0100

    maj

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ if (userText.includes(",")) {
 // ✅ Lancer serveur
 // ===============================
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Serveur démarré sur le port", PORT);
 });
